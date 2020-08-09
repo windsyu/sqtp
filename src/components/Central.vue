6df6daef9e148420b8d78ad64fe2a1d90c2a8aa5
Windsyu <2738064693@qq.com>
2020-08-09 19:47:54 +0800

    暂时完成了以“name”为索引的搜索，将所有数据库返回信息存储在JOSN字符串中

--- src/components/Central.vue
+++ src/components/Central.vue
@@ -59,39 +59,57 @@
 </template>
 
 <script>
+
 import Bmob from "hydrogen-js-sdk";
-var vquery =new Vue({
-el:'#inspire',
-data:{
-  
-}
-});
+var intro;
+var duty;
+var updatedate;
+var name;
+var id;
+var salary;
+var skill;
+var summary;
+var obj1={};
   export default {
     props: {
       source: String,
     },
     data(){
+      intro:''
       return{
         query:'',
-        table:''
       }
     },
     methods:{
       clickHandler(){
         console.log('clk');//测试语句
-        console.log(table);
-        if(this.table==''){
+        console.log(this.query);
+        if(this.query==''){
           console.log('null');
         }
         else{
           const query = Bmob.Query('Article');
-          query.equalTo("name","==",this.table);
-            query.find().then(res => {
-                this.$store.commit("setName",res[0].text);
+          query.equalTo("name","==",this.query);
+         query.find().then(res => {
+            this.$store.commit("setName",res[0].text);
+            intro=JSON.stringify(res[0]);
+            duty=res[0].duty
+            updatedate=res[0].updatedAt
+            name=this.query
+            id=res[0].objectId
+            salary=res[0].salary
+            skill=res[0].skill
+            summary=res[0].summary
+            var jsonObj = JSON.stringify(res[0]);
+            document.write(jsonObj)
+              console.log(res[0].name)  
             }).catch(err => {
                 console.log(err)
             });
-            console.log(query)
+            console.log(query);
+            //document.write(duty)
+            
+
           this.$router.push("/query/"+this.query);
         }
       }
